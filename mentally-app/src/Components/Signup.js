b7f46395f22a0ee6957c62956c84d593d2fb4639
Bailey Conradt <baileyconradt@gmail.com>
2021-09-18 11:25:59 -0500

    added firebase stuff

---
+++ mentally-app/src/Components/Signup.js
@@ -0,0 +1,30 @@
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function Signup() {
+
+
+//     const auth = getAuth();
+
+// function signup(email,password) {
+//     createUserWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//       // Signed in 
+//       const user = userCredential.user;
+//       // ...
+//     })
+//     .catch((error) => {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       // ..
+//     });
+// }
+    
+
+    return (
+        <>
+      <p>Hello this is a signup</p>
+      {/* <button onclick={signup('baileyconradt@gmail.com', 'test123')}>Hello!</button> */}
+      </>
+    );
+    
+  }
